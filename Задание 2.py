e16243372fa7da256fb3419e4b17b30df81b7b83
yarooo007 <97907074+yarooo007@users.noreply.github.com>
2022-01-27 17:49:24 +0300

    Add files via upload

---
+++ Задание 2.py
@@ -0,0 +1,6 @@
+a = 5
+i = 1
+while i<11:
+    s = a*i
+    print(a,"x",i,"=", s)
+    i+=1
---
+++ Задание 4.py
@@ -0,0 +1,9 @@
+a = int(input(""))
+b = int(input(""))
+i = 0
+while i<a:
+    j = 0
+    while j<b:
+        print("*", end=" ")
+        j+=1
+    i+=1
